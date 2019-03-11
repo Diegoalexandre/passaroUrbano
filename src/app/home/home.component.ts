bb643af83100d6b47e3a942e62201a657e73a405
Diego Alexandre Evangelista Da Silva <diego.alexandre@callink.com.br>
2019-03-11 07:45:39 -0300

    Create mock

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { OfertasModel } from '../shared/ofertas.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  ofertas: Array<OfertasModel>;
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    // this.ofertas = this.ofertasService.getOfertas();
+
+    this.ofertasService.getOfertas2()
+      .then((ofertas: OfertasModel[]) => {
+        this.ofertas = ofertas;
+      })
+      .catch((error: any) => console.log(error));
   }
 
 }
---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,71 @@
+import { OfertasModel } from './shared/ofertas.model';
+
+export class OfertasService {
+  ofertas: Array<OfertasModel> = [
+    {
+      id: 1,
+      categoria: 'restaurante',
+      titulo: 'Super Burger',
+      descricao_oferta: 'Rodízio de Mini-hambúrger com opção de entrada.',
+      anunciante: 'Original Burger',
+      valor: 29.90,
+      destaque: true,
+      imagens: [
+        { url: '/assets/ofertas/1/img1.jpg' },
+        { url: '/assets/ofertas/1/img2.jpg' },
+        { url: '/assets/ofertas/1/img3.jpg' },
+        { url: '/assets/ofertas/1/img4.jpg' }
+      ]
+    },
+    {
+      id: 2,
+      categoria: 'restaurante',
+      titulo: 'Cozinha Mexicana',
+      descricao_oferta: 'Almoço ou Jantar com Rodízio Mexicano delicioso.',
+      anunciante: 'Mexicana',
+      valor: 32.90,
+      destaque: true,
+      imagens: [
+        { url: '/assets/ofertas/2/img1.jpg' },
+        { url: '/assets/ofertas/2/img2.jpg' },
+        { url: '/assets/ofertas/2/img3.jpg' },
+        { url: '/assets/ofertas/2/img4.jpg' }
+      ]
+
+    },
+    {
+      id: 4,
+      categoria: 'diversao',
+      titulo: 'Estância das águas',
+      descricao_oferta: 'Diversão garantida com piscinas, trilhas e muito mais.',
+      anunciante: 'Estância das águas',
+      valor: 31.90,
+      destaque: true,
+      imagens: [
+        { url: '/assets/ofertas/3/img1.jpg' },
+        { url: '/assets/ofertas/3/img2.jpg' },
+        { url: '/assets/ofertas/3/img3.jpg' },
+        { url: '/assets/ofertas/3/img4.jpg' },
+        { url: '/assets/ofertas/3/img5.jpg' },
+        { url: '/assets/ofertas/3/img6.jpg' }
+      ]
+    }
+  ];
+
+  public getOfertas(): Array<any> {
+    return this.ofertas;
+  }
+
+  public getOfertas2(): Promise<OfertasModel[]> {
+    return new Promise((resolve, reject) => {
+      const deuCerto = true;
+
+      if (deuCerto) {
+        resolve(this.ofertas);
+      } else {
+        reject({ error: 'Deu errado!!!' });
+      }
+    });
+  }
+
+}
